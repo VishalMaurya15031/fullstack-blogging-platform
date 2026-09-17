f40ea6afd9ca32b13da0f2dd65f4e4c82e1c78ed
vishal <vishalmaurya15031@gmail.com>
2026-09-17 11:32:51 +0530

    Add Vercel serverless deployment config (vercel.json, build_files.sh, wsgi app)

---
+++ build_files.sh
@@ -0,0 +1,6 @@
+echo "BUILD START"
+python -m pip install -r requirements.txt
+python manage.py collectstatic --noinput --clear
+python manage.py migrate --noinput
+python seed_data.py
+echo "BUILD END"
